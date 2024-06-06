bae35b66bf05cb9ba296429753f2487dfdffb5d0
Yurii Polevich <polevichx@gmail.com>
2024-06-07 00:05:23 +0300

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import './App.css';
+import styled from 'styled-components';
+
+
+function App() {
+	return (
+		<div className="App">
+			<Title>Styled-components <span>.attrs</span> method</Title>
+
+			<Form>
+				<Field placeholder='Hello' title='Hi' />
+				<Field type='text' />
+				<Field />
+
+				<Checkbox />
+
+			</Form>
+		</div>
+	);
+}
+
+export default App;
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    margin-bottom: 32px;
+    span {
+      color: #e91e63;
+    }
+`;
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 500px;
+`;
+
+const Field = styled.input.attrs((props) => ({
+	placeholder: props.placeholder || "Type smth...",
+	title: props.title || "Goodbye",
+	type: props.type || "password",
+}))`
+  padding: 5px 15px;
+  margin: 10px 0;
+  width: 100%;
+  font-size: 1rem;
+`;
+
+const Checkbox = styled.input.attrs(() => ({
+	type: "checkbox"
+}))`
+	
+`
